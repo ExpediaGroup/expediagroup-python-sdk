c80edb9241bfede9167e58e53137479421cdb100
Omar Al-Jarrah <oaljarrah@expediagroup.com>
2023-01-15 18:40:38 +0300

    feat(build): Support Dynamic Versioning
    Add support for dynamic versioning parsed from a config file.

--- openworld/sdk/core/setup.py
+++ openworld/sdk/core/setup.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from setuptools import setup
 import configparser
+
+from setuptools import setup
 
 
 def parse_version():
