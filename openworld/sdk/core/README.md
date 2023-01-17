5d9ec71dda26f290b8f34bc284afdbc3419f3c38
Nasser Anssari <nanssari@expediagroup.com>
2023-01-17 14:44:16 +0300

    build: satisfy publish core requirements

---
+++ openworld/sdk/core/README.md
@@ -0,0 +1,3 @@
+# Open World SDK Core Library for Python
+
+The library provides the core functionality for Open World Python SDKs.
--- openworld/sdk/core/setup.py
+++ openworld/sdk/core/setup.py
@@ -16,12 +16,14 @@ import configparser
 
 from setuptools import setup
 
-
 def parse_version():
     config_parser = configparser.ConfigParser()
     config_parser.read("./core.config")
     return config_parser["sdk-properties"]["version"]
 
+def readme():
+    with open("README.md") as f:
+        return f.read()
 
 setup(
     name="openworld-sdk-python-core",
@@ -39,6 +41,23 @@ setup(
     package_dir={"openworld.sdk.core": "."},
     license="Apache License, Version 2.0",
     author="Expedia Group",
+    author_email="oss@expediagroup.com",
+    url="https://github.com/ExpediaGroup/openworld-sdk-python",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 3 :: Only",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires=">=3.7",
     install_requires=["dataclasses_json", "uri", "requests", "python-dateutil"],
-    description="The Open World SDK for Python allows Expedia Group partners to easily build Python applications that leverage the Open World (TM) platform.",
+    description="Open World SDK Core Library for Python",
+    long_description=readme(),
+    long_description_content_type="text/markdown"
 )
