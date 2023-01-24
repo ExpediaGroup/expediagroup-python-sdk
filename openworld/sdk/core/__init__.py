be06d0476da5b049c8d89e39afbd0543c933bc7a
Omar Al-Jarrah <oaljarrah@expediagroup.com>
2023-01-24 03:12:58 +0300

    refactor(logging): Use lastResort as default handler for root logger.

--- openworld/sdk/core/__init__.py
+++ openworld/sdk/core/__init__.py
@@ -13,19 +13,12 @@
 # limitations under the License.
 
 import logging
-from logging.config import fileConfig
 
-try:
-    with open(file="logging.cfg", mode="r+") as f:
-        fileConfig(f)
-except FileNotFoundError:
-    default_logger_handler = logging.StreamHandler()
-    default_formatter = logging.Formatter(
+logging.lastResort.setLevel(logging.NOTSET)
+logging.lastResort.setFormatter(
+    logging.Formatter(
         fmt="[%(asctime)s] %(name)s %(levelname)s: %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
+        datefmt="%Y-%m-%d %H:%M:%S"
     )
-    default_logger_handler.setFormatter(default_formatter)
-
-    default_logger = logging.getLogger(name="openworld")
-    default_logger.addHandler(default_logger_handler)
-    default_logger.setLevel(level=logging.DEBUG)
+)
+logging.root.setLevel(logging.NOTSET)
