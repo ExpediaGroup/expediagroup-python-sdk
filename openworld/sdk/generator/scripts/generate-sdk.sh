fe9d827757524098b1a98735def227e45969e03f
Omar Al-Jarrah <oaljarrah@expediagroup.com>
2023-02-19 16:52:06 +0300

    build(git): Update git workflow to use new generator.

--- openworld/sdk/generator/scripts/generate-sdk.sh
+++ openworld/sdk/generator/scripts/generate-sdk.sh
@@ -52,10 +52,8 @@ while getopts ":n:v:i:" OPTION; do
         esac
 done; validate_arguments
 
-python3 scripts/prepare-spec.py -i "$input_spec"\
-&&\
-scripts/generate-client.sh -i "$(pwd)/specs/spec.yaml" -v "$sdk_version" -n "$sdk_namespace"\
-&&\
-scripts/generate-models.sh -i "$(pwd)/specs/spec.yaml"\
-&&\
+pip3 install -r client/requirements.txt &&\
+python3 scripts/prepare-spec.py -i "$input_spec" &&\
+scripts/generate-client.sh -i "$(pwd)/specs/spec.yaml" -v "$sdk_version" -n "$sdk_namespace" &&\
+scripts/generate-models.sh -i "$(pwd)/specs/spec.yaml" &&\
 scripts/build-package.sh -n "$sdk_namespace"
