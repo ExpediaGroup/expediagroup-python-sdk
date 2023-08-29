bad7fe7a566b15a1622001ce8c98910170d8d9e5
Omar Al-Jarrah <oaljarrah@expediagroup.com>
2023-08-29 17:19:54 +0300

    chore(generator): Update README.md

--- openworld/sdk/generator/client/__main__.py
+++ openworld/sdk/generator/client/__main__.py
@@ -33,6 +33,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+
 from parser import OpenApiParser
 from pathlib import Path
 from typing import Optional
