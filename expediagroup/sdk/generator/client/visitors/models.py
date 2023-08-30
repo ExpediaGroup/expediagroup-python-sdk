b0fcd0e189da379d463b12ce50335dfd7dda20a2
Omar Al-Jarrah <oaljarrah@expediagroup.com>
2023-08-30 15:42:26 +0300

    refactor(generator): Satisfy lint rules.

--- expediagroup/sdk/generator/client/visitors/models.py
+++ expediagroup/sdk/generator/client/visitors/models.py
@@ -14,6 +14,7 @@
 import collections
 import dataclasses
 from pathlib import Path
+from typing import Any
 
 from datamodel_code_generator.imports import Imports
 from datamodel_code_generator.model import DataModel
@@ -227,15 +228,14 @@ def parse_sorted_aliases(models: dict[str, DataModel], discriminators: list[Disc
     return sorted(aliases, key=lambda alias_: alias_.order)
 
 
-def set_error_models_parents(models: dict[str, DataModel], operations: list[Operation]):
+def set_error_models_parents(operations: list[Operation]):
     ok_status_code_range = [code for code in range(200, 300)]
     error_models_classnames: set[str] = set()
+
     for index, operation in enumerate(operations):
-        error_responses: dict = dict()
-        for code, response in operation.additional_responses.items():
-            if int(code) in ok_status_code_range:
-                continue
-            error_responses[int(code)] = response
+        error_responses: dict[int, Any] = {
+            int(code): response for code, response in operation.additional_responses.items() if int(code) not in ok_status_code_range
+        }
 
         operations[index].error_responses = error_responses
         error_models_classnames = error_models_classnames.union(set(map(lambda resp: resp["model"], error_responses.values())))
@@ -257,7 +257,7 @@ def get_models(parser: OpenAPIParser, model_path: Path) -> dict[str, object]:
     models: dict[str, DataModel] = parse_datamodels(parser)
     discriminators: list[Discriminator] = parse_discriminators(parser=parser, models=models)
 
-    set_error_models_parents(models, [operation for operation in parser.operations.values()])
+    set_error_models_parents([operation for operation in parser.operations.values()])
     apply_discriminators_to_models(discriminators=discriminators, models=models)
 
     aliases: list[Alias] = parse_sorted_aliases(models=models, discriminators=discriminators)
