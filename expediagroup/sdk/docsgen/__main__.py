293b28c6874e6f5119643fee0e7c3a5bcd508b19
Omar Al-Jarrah <oaljarrah@expediagroup.com>
2023-10-02 16:32:50 +0300

    Test

--- expediagroup/sdk/docsgen/__main__.py
+++ expediagroup/sdk/docsgen/__main__.py
@@ -27,7 +27,7 @@ app = typer.Typer()
 def main(
     package_name: str = typer.Option(..., "--package-name", "-n"),
     package_path: Path = typer.Option(..., "--package-path", "-p"),
-    templates_path: Optional[Path] = typer.Option(None, "--templates-path", "-t"),
+    templates_path: Optional[Path] = typer.Option("./templates", "--templates-path", "-t"),
     output_path: Optional[Path] = typer.Option(Path(".."), "--output-path", "-o"),
 ):
     print(Path().absolute())
