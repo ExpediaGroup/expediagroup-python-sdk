6e0e447a6cc674f1ecebce92a2c3a25f3db2eac9
Omar Al-Jarrah <oaljarrah@expediagroup.com>
2023-11-05 22:21:00 +0300

    Update log masking based on logging level
    Modified the logging mechanism to introduce log masking based on the logging level. Added a variable `should_logs_masking` to check logging level. This change is done to enhance security by masking logs at DEBUG level or higher and improve memory management.

--- expediagroup/sdk/core/client/api.py
+++ expediagroup/sdk/core/client/api.py
@@ -123,11 +123,11 @@ class ApiClient:
                 timeout=self.request_timeout,
             )
 
-        should_logs_masking: bool = LOG.getEffectiveLevel() >= logging.DEBUG or LOG.getEffectiveLevel() == logging
+        should_logs_masking: bool = LOG.getEffectiveLevel() > logging.DEBUG or LOG.getEffectiveLevel() == logging
 
         logged_body: Union[dict, str, None] = None
         if body:
-            logged_body = json.loads(body.json()) if should_logs_masking else str(body.dict())
+            logged_body = str(body.dict()) if should_logs_masking else json.loads(body.json())
 
         request_log_message = log_util.request_log(
             headers=request_headers,
