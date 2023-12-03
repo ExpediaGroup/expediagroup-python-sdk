a2fc7bf11b8199f85975dc897feea3ded8cbb8d6
Omar Al-Jarrah <oaljarrah@expediagroup.com>
2023-12-03 22:16:59 +0300

    Migrate dev dependencies to test environment
    Unnecessary dependencies removed from the `requirements-dev.txt` file and moved into a newly created `requirements-test.txt` file. Also, redundant imports in `test_api_client.py` have been removed. This refactoring was done to segregate development and testing dependencies, enhancing maintainability.

--- test/core/client/test_api_client.py
+++ test/core/client/test_api_client.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import datetime
-import time
 import unittest
 from test.core.constant import api as api_constant
 from test.core.constant import authentication as auth_constant
