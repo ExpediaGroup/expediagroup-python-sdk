8eb3cf81d4f3c3cc99381b80e29e66fce65aff32
Omar Al-Jarrah <oaljarrah@expediagroup.com>
2023-12-13 16:47:34 +0300

    fix(github): test commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,11 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'header-max-length': [2, 'always', 100],
+        'header-max-length': [2, 'always', 250],
         'type-empty': [2, 'never'],
         'type-case': [2, 'always', 'lower-case'],
         'type-enum': [2, 'always', ['feat', 'refactor', 'fix', 'docs', 'test', 'chore', 'ci', 'release']],
         'scope-empty': [2, 'never'],
-        'scope-case': [2, 'always', 'lower-case'],
         'subject-empty': [2, 'never'],
         'body-leading-blank': [2, 'always'],
         'body-max-line-length': [2, 'always', 100]
