2a9785e9f62199d6b3b9b627fae04f1f7b908747
Omar Aljarrah <50204418+OmarAlJarrah@users.noreply.github.com>
2023-12-13 16:58:35 +0300

    feat(github): fix not latest commit only validated (#300)
    PR: https://github.com/ExpediaGroup/expediagroup-python-sdk/pull/300

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [2, 'always', 250],
+        'type-empty': [2, 'never'],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-enum': [2, 'always', ['feat', 'refactor', 'fix', 'docs', 'test', 'chore', 'ci', 'release']],
+        'subject-empty': [2, 'never'],
+        'body-leading-blank': [2, 'always'],
+        'body-max-line-length': [2, 'always', 100]
+    }
+};
