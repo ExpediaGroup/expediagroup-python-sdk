388696503c567e596c4c20fc99d6e7dd16ec5f78
Mohammad Noor Abu Khleif <moh.noor94@gmail.com>
2023-12-24 16:40:43 +0300

    chore: update subject case commit rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,6 +6,7 @@ module.exports = {
         'type-case': [2, 'always', 'lower-case'],
         'type-enum': [2, 'always', ['feat', 'refactor', 'fix', 'docs', 'test', 'chore', 'ci', 'release', 'build']],
         'subject-empty': [2, 'never'],
+        'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
         'body-leading-blank': [2, 'always'],
         'body-max-line-length': [2, 'always', 100]
     }
